4c45d50ace861c6da78766d63d88a267b085ae54
Max Maximov <mr.neonin@gmail.com>
2020-09-25 19:48:10 +0300

    added echo bot

--- index.js
+++ index.js
@@ -1,61 +1,22 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.use(async (ctx, next) => {
-    ctx.reply('Mddleware')
-    // ctx.state.isSend = true
-    // ctx.state.user = 'Test'
-
-    // throw 'some error'
-
-    await next(ctx)
+bot.start(ctx => {
+    ctx.reply('Hello! Its echo bot!')
 })
 
-bot.catch((err, ctx) => {
-    console.log('ERROR', err)
+bot.help(ctx => {
+    ctx.reply('Send any message and I will copy it')
 })
 
-// commands
-bot.start((ctx) => {
-    const {state} = ctx
-    console.log(state)
-    return ctx.reply(`Start command ${state.isSend}`)
-})
-bot.help(ctx => ctx.reply('Help command'))
-bot.settings(ctx => ctx.reply('Settings'))
-// command method
-bot.command(['stop', 'finish'], ctx => {
-    return ctx.reply('Stop comand')
+bot.on('message', ctx => {
+    ctx.telegram.sendCopy(ctx.chat.id, ctx.message)
 })
 
-bot.mention('botfather', (ctx) => {
-    ctx.reply('botfather mention')
-})
-
-bot.phone('+380676029700', (ctx) => {
-    ctx.reply('phone number')
-})
-
-bot.hashtag('bot', (ctx) => {
-    ctx.reply('bot hashtag')
-})
-// context test
-bot.command('ctx', (ctx) => {
-    console.log(ctx.update)
-    ctx.reply(`${ctx.update.message.from.first_name}, hello`)
-})
-
-bot.hears('dog', ctx => {
-    ctx.reply('who lets the dog out')
-})
-
-bot.on(['message', 'edited_message'], ctx => {
-    console.log(ctx.updateType)
-    console.log(ctx.updateSubTypes)
-})
-bot.launch().then(res => {
-    console.log('Start')
-}).catch((err) => {
+bot.launch().then((res) => {
+    console.log('run')
+}).catch(err => {
     console.log(err)
 })
